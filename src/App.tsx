ca8e07d60745e28ea3b921d2e8cd6bc5f9df9cae
wushijie <wushijie@melot.cn>
2023-06-20 14:13:27 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+
+const App = () => {
+    return <div className="app"></div>
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3001
+    },
+    resolve: {
+        alias: {
+            // 必须写成'/src/'这种绝对路径的形式，否则无效
+            "~/": "/src/"
+        }
+    }
+})
