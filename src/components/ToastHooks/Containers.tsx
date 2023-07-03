2445bd6db4c0667916e1764fbf8f394a86ca2110
guozi007a <1208001055@qq.com>
2023-07-04 01:31:04 +0800

    上下文提示

---
+++ src/components/ToastHooks/Containers.tsx
@@ -0,0 +1,8 @@
+/** 承载通知的容器s */
+import styles from './containers.module.scss'
+
+const Containers = () => {
+    return <div className={styles.containers}></div>
+}
+
+export default Containers
---
+++ src/components/ToastHooks/Notify.tsx
@@ -0,0 +1,8 @@
+/** 单条通知 */
+import styles from './notify.module.scss'
+
+const Notify = () => {
+    return <div className={styles.notify}></div>
+}
+
+export default Notify
