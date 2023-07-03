baa7ca13ad1368822e39588ff90fe67f0d1c8282
guozi007a <1208001055@qq.com>
2023-07-04 01:05:02 +0800

    修改文案

--- src/pages/Toast/index.tsx
+++ src/pages/Toast/index.tsx
@@ -9,7 +9,7 @@ const Toast = () => {
         <Tiny.TinyTitle1 title='轻提示' />
         <Tiny.TinyText>
             在项目中，点击按钮或调用接口时，会向用户提供活动开启、活动结束、活动进度、是否中奖、开奖奖励、金额不足、金额超限等提示。<br />
-            这些提示通常调用<Tiny.TinyTag tag='toast' />方法来完成。该方法提供了默认、普通、成功、失败和警告等多种形式的轻提示。
+            这些提示通常调用<Tiny.TinyTag tag='toast' />方法或者默认的<Tiny.TinyTag tag='showToast' />方法来完成。该方法提供了默认、普通、成功、失败和警告等多种形式的轻提示。
         </Tiny.TinyText>
         <Tiny.TinyTitle2 title='全局轻提示' />
         <Tiny.TinyText>
