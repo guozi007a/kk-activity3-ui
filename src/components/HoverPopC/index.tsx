0055967d843ea91dc7eb04b2bd2873b0f956d70d
guozi007a <1208001055@qq.com>
2023-07-05 23:30:11 +0800

    add: hover pop

---
+++ src/components/HoverPopC/index.tsx
@@ -0,0 +1,12 @@
+/** 悬浮框 */
+import styles from './index.module.scss'
+
+const HoverPopC = () => {
+    return <div className={styles.hover_pop}>
+        <main className={styles.stone}>
+            <section className={styles.mirror}></section>
+        </main>
+    </div>
+}
+
+export default HoverPopC
--- src/pages/HoverPop/index.tsx
+++ src/pages/HoverPop/index.tsx
@@ -1,13 +1,18 @@
 /** 悬浮框 web端悬浮展示 手机端点击展示 */
 import styles from './index.module.scss'
 import * as Tiny from '~/components/Tinys'
+import HoverPopC from '~/components/HoverPopC'
 
 const HoverPop = () => {
     return <div className={styles.hover_pop}>
         <Tiny.TinyTitle1 title='悬浮框' />
         <Tiny.TinyText>
-            web端悬浮展示 手机端点击展示。通常用于展示礼物奖励、规则小提示等。
+            web端悬浮展示，手机端点击展示。通常用于展示礼物奖励、规则小提示等。
         </Tiny.TinyText>
+        <Tiny.TinyTitle2 title='效果展示' />
+        <div className={styles.show_pop}>
+            <HoverPopC />
+        </div>
     </div>
 }
 
