6569324597ddf7c4e7123771b3c26bc061a19f30
guozi007a <1208001055@qq.com>
2023-07-06 21:18:46 +0800

    fix: 修改自适应库引入

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import '~/assets/css/common.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from '~/router'
 import 'md-editor-rt/lib/style.css'
+import './utils/flexible'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
