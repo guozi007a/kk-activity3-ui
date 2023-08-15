ffc4dc0d83e90d7ac8be69780927d643f40f0af9
guozi007a <1208001055@qq.com>
2023-08-16 07:33:20 +0800

    feat: 添加定制滚动条

--- src/components/ScrollbarC/index.tsx
+++ src/components/ScrollbarC/index.tsx
@@ -5,14 +5,31 @@ interface PropsConfig {
     children?: React.ReactNode
     // 不滚动时，自动隐藏滚动条，默认true
     autoHide?: boolean
+    // 隐藏动画持续时间，单位ms，默认300
+    autoHideDuration?: number
+    // 纵向滚动条的高度，或者横向滚动条的宽度，单位px
+    thumbSize?: number
+    // 滚动条style样式
+    thumbStyle?: React.CSSProperties
 }
 
 const ScrollbarC = ({
     children,
     autoHide = true,
+    autoHideDuration = 300,
+    thumbSize,
+    thumbStyle,
 }: PropsConfig) => {
+
+    const renderThumb = () => {
+        return <div style={thumbStyle}></div>
+    }
     return <Scrollbars
         autoHide={autoHide}
+        autoHideDuration={autoHideDuration}
+        thumbSize={thumbSize}
+        renderThumbHorizontal={thumbStyle && renderThumb}
+        renderThumbVertical={thumbStyle && renderThumb}
     >
         {children}
     </Scrollbars>
