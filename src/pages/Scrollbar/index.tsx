20bd32e565453668575c66f422092dfe64ebcdeb
wushijie <wushijie@melot.cn>
2023-08-16 11:39:19 +0800

    添加滚动条

--- src/pages/Scrollbar/index.tsx
+++ src/pages/Scrollbar/index.tsx
@@ -2,6 +2,7 @@
 import styles from './index.module.scss'
 import * as Tiny from '~/components/Tinys'
 import ScrollbarC from '~/components/ScrollbarC'
+import Controller from '~/components/Controller'
 
 const Scrollbar = () => {
     return <div className={styles.scrollbar_wrap}>
@@ -15,6 +16,45 @@ const Scrollbar = () => {
                 <div className={styles.demo_main}>滚动吧，骚年！</div>
             </ScrollbarC>
         </div>
+        <Tiny.TinyTitle2 title='控制器' />
+        <Controller>
+            <tbody>
+                <tr>
+                    <td>滚动区内容</td>
+                    <td>children</td>
+                    <td>React.ReactNode</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>自动隐藏滚动条</td>
+                    <td>autoHide</td>
+                    <td>boolean</td>
+                    <td>true</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>隐藏动画持续时间</td>
+                    <td>autoHideDuration</td>
+                    <td>number</td>
+                    <td>300</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>滚动条的宽度</td>
+                    <td>thumbSize</td>
+                    <td>number</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>滚动条style样式</td>
+                    <td>thumbStyle</td>
+                    <td>React.CSSProperties</td>
+                    <td>null</td>
+                    <td>-</td>
+                </tr>
+            </tbody>
+        </Controller>
     </div>
 }
 
